3e5ef9624847dfc386c8b1cd74884712ea12cf8d
vikrantaroraa <arora.veshu@gmail.com>
2024-01-15 20:24:39 +0530

    refactor: deleted storybook bcoz of warnings, modified noUnusedLocals value and changed paths to relative from absolute

--- src/components/MyButton/index.tsx
+++ src/components/MyButton/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ButtonProps } from "src/components/MyButton/index.interface";
-import styles from "src/components/MyButton/index.module.css";
+import { ButtonProps } from "./index.interface";
+import styles from "./index.module.css";
 
 // See how to implement ...rest parameters type in the interface.
 function MyButton({ children, onClick, style, type }: ButtonProps) {
